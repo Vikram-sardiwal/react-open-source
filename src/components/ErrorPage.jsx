f3a0bb4a111ac9da99768ab4a89762bd5654f8f7
vikash chaudhary <vikashbrilliant1@gmail.com>
2026-08-05 22:04:51 +0530

    updated image alt attribute and fixed typos

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div class="error-container">
-        <img src="/error-emoji.jpg" alt="" />
-        <div class="error-text">
-            <div class="error-heading">error  404</div>
-            <div class="error-sub">Oops! Page not found</div>
+    <div className="error-container">
+        <img src="/error-emoji.jpg" alt="error-image" />
+        <div className="error-text">
+            <div className="error-heading">error  404</div>
+            <div className="error-sub">Oops! Page not found</div>
             
 
         </div>
-        <Link to={"/"} class="home-button">← Go Home</Link>
+        <Link to={"/"} className="home-button">← Go Home</Link>
       
     </div>
   )
