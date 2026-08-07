46ad13f291cb702385eb7f122ac4b59fa1c6a19e
L-on-the-Web <06ljackson@gmail.com>
2026-08-06 22:30:51 -0400

    feat: add dark mode toggle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Welcome from './components/Welcome.jsx'
+import DarkMode from './components/Dark-Mode.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
+      <DarkMode />
       <Welcome />
     </div>
   )
