6683631e4425cb49acfc6cbd78786d20fa1673e0
Saumya Verma <saumyaverma46@gmail.com>
2026-08-10 12:18:05 +0530

    fix: improve application accessibility

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,21 +1,16 @@
-
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="error-container">
-        <img src="/error-emoji.jpg" alt="error-image" />
-        <div className="error-text">
-            <div className="error-heading">error  404</div>
-            <div className="error-sub">Oops! Page not found</div>
-            
+    <main className="error-page">
+      <h1>404</h1>
+      <h2>Oops! Page not found</h2>
 
-        </div>
-        <Link to={"/"} className="home-button">← Go Home</Link>
-      
-    </div>
-  )
-}
+      <Link to="/" className="home-button">
+        ← Go Home
+      </Link>
+    </main>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
