96d5159a07a717df16f0e55236d78fcd0921fbb1
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-08-11 14:58:33 +0530

    Fix footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,13 @@ function Footer() {
         <h3>React Open Source Starter</h3>
 
         <nav className="footer-links">
-         <a href="/">Home</a>
-  <a href="https://github.com/Vikram-sardiwal/react-open-source">GitHub</a>
-  <a href="https://github.com/Vikram-sardiwal/react-open-source/blob/main/CONTRIBUTING.md">
-    Contributing
-  </a>
+          <a href="/">Home</a>
+          <a href="https://github.com/Vikram-sardiwal/react-open-source">
+            GitHub
+          </a>
+          <a href="https://github.com/Vikram-sardiwal/react-open-source/blob/main/CONTRIBUTING.md">
+            Contributing
+          </a>
         </nav>
 
         <p>© {new Date().getFullYear()} React Open Source Starter</p>
@@ -19,3 +21,4 @@ function Footer() {
 }
 
 export default Footer;
+
