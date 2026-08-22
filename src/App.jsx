28552a9cc0ee1c97c9765bf76505e418ffce54f2
vidhya12032000 <vidhyaamutha1231@gmail.com>
2026-08-23 02:37:39 +0530

    Improve landing page UI and contribution section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import Welcome from "./components/Welcome.jsx";
+import Welcome from "./components/Welcome/Welcome.jsx";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage.jsx";
 import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route
           path="/"
           element={
             <>
               <div className="app">
-                <Welcome />
+                <Home />
               </div>
               <Footer />
             </>
